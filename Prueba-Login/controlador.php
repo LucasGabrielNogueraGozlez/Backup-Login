ac7b4b9167d62d7d2299cbc26d15a3a548dbb268
LucasGabrielNogueraGozlez <141255262+LucasGabrielNogueraGozlez@users.noreply.github.com>
2023-08-10 09:27:52 -0400

    Update controlador.php

--- Prueba-Login/controlador.php
+++ Prueba-Login/controlador.php
@@ -1,19 +1,20 @@
+
 <?php
+require "./conexion_bd.php";
 if(!empty($_POST["btningresar"])) {
     if(empty($_POST["usuario"]) or empty($_POST["password"])) {
-   echo '<div class="compl_campos">LOS CAMPOS ESTAN VACIOS</div>';
-} else {
-$usuario=$_POST['usuario'];
-$clave=$_POST['password'];
-$sql=$conex->query(" select * from usuarios where usuario='$usuario' and clave='$clave' ");
-if ($datos=$sql->fetch_object()) {
-    header("location:index.php");
-} else {
-    echo '<div class="acce_deneg">ACCESO DENEGADO</div>';
-}
-    }  
-
-
+        echo '<div class="compl_campos">COMPLETE LOS CAMPOS</div>';
+    } else {
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['password'];
+        $sql = $conex->query("select * from usuarios where usuario='$usuario' and clave='$clave' ");
+        if ($datos = $sql->fetch_object()) {
+            header("location:index.php"); 
+            exit(); 
+        } else {
+            echo '<div class="acce_deneg">ACCESO DENEGADO</div>';
+        }
+    }
 }
 
 ?>
