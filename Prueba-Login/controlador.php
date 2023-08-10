7137991a127164cfaef113886af34aba27c47f05
José Ramírez <138532719+kuuukooo@users.noreply.github.com>
2023-08-10 10:11:09 -0400

    Minúsculas
    letras en minusculas, solo eso

--- Prueba-Login/controlador.php
+++ Prueba-Login/controlador.php
@@ -3,7 +3,7 @@
 require "./conexion_bd.php";
 if(!empty($_POST["btningresar"])) {
     if(empty($_POST["usuario"]) or empty($_POST["password"])) {
-        echo '<div class="compl_campos">COMPLETE LOS CAMPOS</div>';
+        echo '<div class="compl_campos">Complete los campos</div>';
     } else {
         $usuario = $_POST['usuario'];
         $clave = $_POST['password'];
@@ -12,7 +12,7 @@ if(!empty($_POST["btningresar"])) {
             header("location:index.php"); 
             exit(); 
         } else {
-            echo '<div class="acce_deneg">ACCESO DENEGADO</div>';
+            echo '<div class="acce_deneg">Acceso Denegado</div>';
         }
     }
 }
